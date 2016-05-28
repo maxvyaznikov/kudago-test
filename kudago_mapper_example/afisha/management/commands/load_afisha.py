2c974ea6f33ac0a8af6f38b2e4355c9dd826e2fd
Max Vyaznikov <maxvyaznikov@gmail.com>
2016-05-28 11:21:48 +0300

    Loader of data from external source (currently only for Event model) realized as management command

---
+++ kudago_mapper_example/afisha/management/commands/load_afisha.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from kudago_mapper.xml import XmlFileMapper
+
+from afisha.mapper import TestXmlEventTransform
+
+
+class Command(BaseCommand):
+    help = 'Example of loading data for afisha package from external sources'
+
+    def handle(self, *args, **options):
+        # Dicts to keep relationship between objects from Source and objects
+        # in DB
+        events, places = {}, {}
+        source = XmlFileMapper(source_file=settings.TEST_SOURCE_FILE_PATH)
+        source.register_transform(TestXmlEventTransform(events))
+        # source.register_transform(TestXmlPlaceTransform(places))
+        source.load()
