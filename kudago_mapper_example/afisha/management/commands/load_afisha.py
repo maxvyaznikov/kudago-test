314a692df8a05b4742d3d21e73a28fc24470277e
Max Vyaznikov <maxvyaznikov@gmail.com>
2016-05-28 15:27:54 +0300

    Update events if it's already exists in DB instead of creating a new one.

--- kudago_mapper_example/afisha/management/commands/load_afisha.py
+++ kudago_mapper_example/afisha/management/commands/load_afisha.py
@@ -2,16 +2,18 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from kudago_mapper.xml_mapper import XmlFileMapper
 
-from afisha.mapper import TestXmlEventTransform
+from afisha.mapper.test_xml.event_mapper import TestXmlEventTransform
+from afisha.models import Event, Place
 
 
 class Command(BaseCommand):
     help = 'Example of loading data for afisha package from external sources'
 
     def handle(self, *args, **options):
-        # Dicts to keep relationship between objects from Source and objects
-        # in DB
-        events, places = {}, {}
+        # Dicts to keep relationship between objects from external source and
+        # objects in DB
+        events = {event.external_id: event for event in Event.objects.all()}
+        places = {place.external_id: place for place in Place.objects.all()}
         source = XmlFileMapper(source_file=settings.TEST_SOURCE_FILE_PATH)
         source.register_transform(TestXmlEventTransform(events))
         # source.register_transform(TestXmlPlaceTransform(places))
